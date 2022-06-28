e2687da5f9b921158ab3f64ac3754ca88e9a4756
佐藤魁 <satokai@satoukainoMacBook-Pro.local>
2022-06-29 01:40:34 +0900

    ライブラリ追加

--- server.js
+++ server.js
@@ -1,1 +1,8 @@
-console.log("start");
+const express = require("express");
+const app = express();
+
+const PORT = 3000;
+
+app.use(express.static("public"));
+
+app.listen(PORT, () => console.log("server start!"));
